23389699fc7bba83434d22524ebc73471bc0bb00
Albert I <kras@raphielgang.org>
2019-06-12 00:11:19 +0800

    scripts: Major cleanup
    Basically too many things changed to be explained, but well...
    Hope it doesn't break anything.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- setup/termux.bash
+++ setup/termux.bash
@@ -1,12 +1,13 @@
+# shellcheck shell=bash
 # Termux environment setup
 # Copyright (C) 2019 Albert I (krasCGQ)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Exit setup if encountering any errors
-trap 'exit ${?}' ERR
+trap 'exit $?' ERR
 
 # Move to HOME if it isn't
-[[ $(pwd) != ${HOME} ]] && cd ${HOME}
+[[ $(pwd) != "$HOME" ]] && cd "$HOME" || exit 1
 
 # Variables here are basically to define upstream file names
 ANTIBODY=antibody_Linux_arm64.tar.gz
@@ -18,13 +19,13 @@ pkg install git nano zsh -y
 git clone https://gitlab.com/krasCGQ/dotfiles .files --single-branch
 mkdir -p .config
 for i in .config/nano .gitconfig; do
-    ln -sf ${HOME}/.files/${i} ${HOME}/${i}
+    ln -sf "$HOME"/.files/$i "$HOME"/$i
 done
-ln -sf ${HOME}/.files/.zshrc-android ${HOME}/.zshrc
+ln -sf "$HOME"/.files/.zshrc-android "$HOME"/.zshrc
 
 # antibody
-curl -LO https://github.com/getantibody/antibody/releases/latest/download/${ANTIBODY}
-tar -xf ${ANTIBODY} antibody
+curl -LO https://github.com/getantibody/antibody/releases/latest/download/$ANTIBODY
+tar -xf $ANTIBODY antibody
 mkdir -p ../usr/local/bin
 mv -f antibody ../usr/local/bin
 
