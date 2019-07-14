8e8990e055be7f2cd00824f0b2630f6c527de544
Albert I <kras@raphielgang.org>
2019-07-14 14:38:27 +0800

    utils: Public bringup of Kernel.org release announcement script
    * Parses release RSS for new updates.
    * Saves a file for each releases found to ~/.korg-announce/ for
      verification purposes using SHA-384. This avoids announcing same version
      multiple times.
    * Announces only release type, version, release date and changelog (non-
      mainline).
    
    The output be seen on Telegram as KudNotifier (https://t.me/KudNotifier).
    
    Signed-off-by: Albert I <kras@raphielgang.org>

---
+++ utils/korg-announce.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Albert I (krasCGQ)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from contextlib import suppress
+from feedparser import parse
+from hashlib import sha384
+from os import environ, mkdir, remove
+from os.path import exists, join, isdir
+from requests import post
+
+def notify(msg):
+    token = environ['TELEGRAM_TOKEN']
+    chat_id = environ['TELEGRAM_CHAT']
+    tg_url = 'https://api.telegram.org/bot' + token + '/SendMessage'
+    query = { 'chat_id': chat_id, 'text': msg, 'parse_mode': 'Markdown' }
+
+    return post(tg_url, data=query)
+
+def main():
+    korg_url = 'https://www.kernel.org/feeds/kdist.xml'
+    list = parse(korg_url)
+
+    for i in range (0, len(list.entries)):
+        # skip linux-next; we only want stable and mainline releases
+        if 'linux-next' not in list.entries[i].title:
+            details = list.entries[i].id.split(',')
+            if 'mainline' in list.entries[i].title:
+                release = details[2].split('-')
+                release = release[0].split('.')
+            else:
+                release = details[2].split('.')
+            version = release[0] + '.' + release[1]
+            version_file = join(path + '/' + version + '-version')
+
+            if exists(version_file):
+                file = open(version_file, 'rb')
+                hash_a = sha384(file.read()).hexdigest()
+                file.close()
+            else:
+                hash_a = ''
+
+            hash_b = sha384(str.encode(list.entries[i].title)).hexdigest()
+
+            file = open(version_file, 'w+')
+            file.write(list.entries[i].title)
+            file.close()
+
+            if hash_a != hash_b:
+                if 'mainline' in list.entries[i].title:
+                    msg = '*New Linux mainline release available!*\n'
+                else:
+                    msg = '*New Linux ' + version + ' series release available!*\n'
+                    msg += 'Release type: ' + details[1] + '\n'
+                msg += 'Version: ' + details[2] + '\n'
+                msg += 'Release date: ' + details[3]
+                if 'mainline' not in list.entries[i].title:
+                    msg += '\n\n'
+                    msg += '[Changes from previous release](https://cdn.kernel.org/pub/linux/kernel/v' + release[0] + '.x/ChangeLog-' + details[2] + ')'
+
+                notify(msg)
+
+if __name__ == '__main__':
+    path = environ['HOME'] + '/.korg-announce'
+
+    with suppress(FileExistsError):
+        if not isdir(path):
+            with suppress(FileNotFoundError):
+                remove(path)
+
+        mkdir(path)
+
+    main()
