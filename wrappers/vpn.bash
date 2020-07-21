815b5adbb7dce67d5cf3091e07b1f5614b2f6e7e
Albert I <kras@raphielgang.org>
2020-07-22 03:38:32 +0800

    wrappers/vpn: Expand to include WireGuard interfaces
    Provided glob should match even one created by third party VPN client.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- wrappers/vpn.bash
+++ wrappers/vpn.bash
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 # VPN status wrapper for i3status (i3status-rust preferred)
-# Copyright (C) 2019 Albert I (krasCGQ)
+# Copyright (C) 2019-2020 Albert I (krasCGQ)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Default display
 STATUS="VPN "
 
-# Check if tun0 is exist
-[[ -d /proc/sys/net/ipv4/conf/tun0 ]] && STATUS+="" || STATUS+=""
+# Check if one of the following interfaces is exist
+# wg* should match even WireGuard interface made by third party VPN
+NET=/proc/sys/net/ipv4/conf
+for i in $NET/tun0 "$NET"/wg*; do
+    [[ -d $i ]] && UP=true && break
+done
+[[ -n $UP ]] && STATUS+="" || STATUS+=""
 
 # Final output
 echo "${STATUS}"
