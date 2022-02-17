4952ba4ddf18b15f13a9b78c6e6a63c0046ad57b
Albert I <kras@raphielgang.org>
2022-02-17 22:05:25 +0800

    utils/notifier: Convert Git releases notifier to a module [3/4]

---
+++ utils/notifier/git.py
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2019-2022 Albert I (krasCGQ)
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+from os import makedirs as os_makedirs
+from os.path import exists as path_exists, join as path_join
+
+from git import cmd as git_cmd
+
+from notifier import utils
+
+def announce(path):
+    # initialize GitPython
+    git = git_cmd.Git()
+    # list of urls to announce
+    url = ['git://git.zx2c4.com/wireguard-linux-compat']
+
+    # for each url...
+    for i in range (0, len(url)):
+        # repository name
+        repo = url[i].split('/')[-1]
+        # list of tags
+        tags = git.ls_remote('--tags', url[i]).split('\n')
+
+        repo_path = path_join(path + '/' + repo)
+        # create repo directory if not exists
+        if not path_exists(repo_path):
+            os_makedirs(repo_path)
+
+        # parse every 2 entries, next one is tagged commit
+        for j in range (0, len(tags), 2):
+            tag = tags[j].replace('/', '\t').split('\t')
+            tag_file = path_join(repo_path + '/' + tag[3])
+            # short SHA-1 format – first 12 letters
+            tag_sha = tag[0][:12]
+
+            # although rare since tag re-releases are uncommon, announce if tag is different
+            if utils.read_from_file(tag_file) != tag_sha:
+                msg = '*New Git release detected!*\n'
+                msg += '\n'
+                msg += 'Repository: [' + repo + '](' + url[i].replace('git:', 'https:') + ')' + '\n'
+                msg += 'Tag: `' + tag[3] + '` (`' + tag_sha + '`)\n'
+                msg += 'Commit: `' + tags[j + 1][:12] + '`'
+
+                utils.push_notification(msg)
+                # write tag sha
+                utils.write_to_file(tag_file, tag_sha)
+
--- utils/tg-announce.py
+++ utils/tg-announce.py
@@ -8,47 +8,8 @@ from os.path import exists, join, isdir
 
 from argparse import ArgumentParser
 from feedparser import parse
-from git import cmd as git_cmd
 
-from notifier import linux, utils
-
-# git release announcement
-def git_announce():
-    # initialize GitPython
-    git = git_cmd.Git()
-    # list of urls to announce
-    url = ['git://git.zx2c4.com/wireguard-linux-compat']
-
-    # for each url...
-    for i in range (0, len(url)):
-        # repository name
-        repo = url[i].split('/')[-1]
-        # list of tags
-        tags = git.ls_remote('--tags', url[i]).split('\n')
-
-        repo_path = join(path + '/' + repo)
-        # create repo directory if not exists
-        if not exists(repo_path):
-            makedirs(repo_path)
-
-        # parse every 2 entries, next one is tagged commit
-        for j in range (0, len(tags), 2):
-            tag = tags[j].replace('/', '\t').split('\t')
-            tag_file = join(repo_path + '/' + tag[3])
-            # short SHA-1 format – first 12 letters
-            tag_sha = tag[0][:12]
-
-            # although rare since tag re-releases are uncommon, announce if tag is different
-            if utils.read_from_file(tag_file) != tag_sha:
-                msg = '*New Git release detected!*\n'
-                msg += '\n'
-                msg += 'Repository: [' + repo + '](' + url[i].replace('git:', 'https:') + ')' + '\n'
-                msg += 'Tag: `' + tag[3] + '` (`' + tag_sha + '`)\n'
-                msg += 'Commit: `' + tags[j + 1][:12] + '`'
-
-                utils.push_notification(msg)
-                # write tag sha
-                utils.write_to_file(tag_file, tag_sha)
+from notifier import git, linux, utils
 
 # projects (SourceForge, OSDN File Storage) announcement
 def project_announce():
@@ -122,7 +83,7 @@ if __name__ == '__main__':
         makedirs(path)
 
     if args.type == 'git':
-        git_announce()
+        git.announce(path)
     elif args.type == 'linux':
         linux.announce(path)
     elif args.type == 'project':
