5ccf7448b1fbff7da8515f06c4e52d5506f5617f
Albert I <kras@raphielgang.org>
2022-02-17 23:44:23 +0800

    utils/notifier: Only load module required for announcement

--- utils/notifier.py
+++ utils/notifier.py
@@ -5,20 +5,20 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
+from importlib import import_module
 from os import environ as os_environ, makedirs as os_makedirs, remove as os_remove
 from os.path import exists as path_exists, isdir as path_isdir, join as path_join
 
 from argparse import ArgumentParser
 
-from notifier import git, linux, project, utils
-
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='All-in-one Telegram announcement script using Telegram Bot API.')
-    parser.add_argument('-t', '--type', help='select announcement type desired',
-                        type=str, choices=['git', 'linux', 'project'])
+    parser.add_argument('-t', '--type', help='select announcement type desired', type=str)
 
     args = parser.parse_args()
+
+    notifier = import_module(f'.{args.type}', 'notifier')
 
     path = path_join(os_environ['HOME'] + '/.tg-announce/')
     # attempt removal of file of same name
@@ -30,9 +30,4 @@ if __name__ == '__main__':
     if not path_exists(path):
         os_makedirs(path)
 
-    if args.type == 'git':
-        git.announce(path)
-    elif args.type == 'linux':
-        linux.announce(path)
-    elif args.type == 'project':
-        project.announce(path)
+    notifier.announce(path)
