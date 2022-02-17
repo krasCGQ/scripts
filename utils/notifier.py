8d78ea6ef0392cc8e34f0eefc986f7152d630088
Albert I <kras@raphielgang.org>
2022-02-17 22:27:59 +0800

    utils/notifier: Rename main notifier script

--- utils/notifier.py
+++ utils/notifier.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-# Copyright (C) 2019-2021 Albert I (krasCGQ)
+#
+# Copyright (C) 2019-2022 Albert I (krasCGQ)
+#
 # SPDX-License-Identifier: GPL-3.0-or-later
+#
 
-from os import environ, makedirs, remove
-from os.path import exists, join, isdir
+from os import environ as os_environ, makedirs as os_makedirs, remove as os_remove
+from os.path import exists as path_exists, isdir as path_isdir, join as path_join
 
 from argparse import ArgumentParser
 
 from notifier import git, linux, project, utils
 
-# main functions
+
 if __name__ == '__main__':
     parser = ArgumentParser(description='All-in-one Telegram announcement script using Telegram Bot API.')
     parser.add_argument('-t', '--type', help='select announcement type desired',
@@ -17,15 +20,15 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    path = join(environ['HOME'] + '/.tg-announce/')
+    path = path_join(os_environ['HOME'] + '/.tg-announce/')
     # attempt removal of file of same name
-    if exists(path) and not isdir(path):
-        remove(path)
+    if path_exists(path) and not path_isdir(path):
+        os_remove(path)
 
-    path = join(path + args.type)
+    path = path_join(path + args.type)
     # create cache directory if not exists
-    if not exists(path):
-        makedirs(path)
+    if not path_exists(path):
+        os_makedirs(path)
 
     if args.type == 'git':
         git.announce(path)
