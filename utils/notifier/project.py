1a670df6137ac4a19c954a39da5be22e49d59adb
Albert I <kras@raphielgang.org>
2023-11-24 23:34:11 +0800

    notifier: project: Some maintenance tasks before actual changes

--- utils/notifier/project.py
+++ utils/notifier/project.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2022 Albert I (krasCGQ)
+# Copyright (C) 2019-2023 Albert I (krasCGQ)
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
@@ -15,36 +15,38 @@ from notifier import config, utils
 
 def announce(path: str, dry_run: bool):
     for i in range(0, len(config.project_lists)):
+        project: str
+        service: str
         # parse each project and service
-        project: str = config.project_lists[i].split(':')[0]
-        service: str = config.project_lists[i].split(':')[1]
+        [project, service, *_] = config.project_lists[i].split(':')
 
         # url of the project
-        project_url: str = 'https://' + service + '.net/projects/' + project
-        rss_url: str
+        project_url: str = 'https://{}.net/projects/{}'.format(service, project)
+        project_rss: str
         if service == 'sourceforge':
-            rss_url = project_url + '/rss'
+            project_rss = '{}/rss'.format(project_url)
         elif service == 'osdn':
-            rss_url = project_url + '/storage/!rss'
+            project_rss = '{}/storage/!rss'.format(project_url)
         else:
             # error out
-            raise Exception(service + " isn't a valid service. Valid services: sourceforge, osdn.")
+            raise Exception(
+                '{} is not a supported service. Valid services: sourceforge, osdn.'.format(service))
 
-        list = feedparser_parse(rss_url)
+        list = feedparser_parse(project_rss)
 
         # start from the oldest
         for j in range(len(list.entries) - 1, -1, -1):
             # get the file name instead of full path
-            name: str = list.entries[j].title.split('/')[-1]
+            file_name: str = list.entries[j].title.split('/')[-1]
             digest: str = hashlib_sha384(list.entries[j].title.encode()).hexdigest()
 
-            service_path: str = path_join(path + '/' + service)
+            service_path: str = path_join('{}/{}'.format(path, service))
             # create service directory if not exists
             if not path_exists(service_path):
                 os_makedirs(service_path)
 
             # cache file: use file name
-            cache_file: str = path_join(service_path + '/' + name)
+            cache_file: str = path_join('{}/{}'.format(service_path, file_name))
             # both hashes are different, announce it
             if utils.get_digest_from_content(cache_file) != digest:
                 message: str
@@ -53,14 +55,14 @@ def announce(path: str, dry_run: bool):
                 elif service == 'osdn':
                     message = '*New file detected on OSDN File Storage:* [' + project + '](' + project_url + ')\n'
                 message += '\n'
-                message += 'Name: `' + name + '`\n'  # avoid markdown parsing
+                message += 'Name: `' + file_name + '`\n'  # avoid markdown parsing
                 message += 'Upload date: ' + list.entries[j].published + '\n'
                 message += '\n'
                 if service == 'sourceforge':
                     message += '[Download](' + list.entries[j].link + ')'
                 elif service == 'osdn':
                     # use shortlink provided by OSDN
-                    message += '[Download](https://' + service + '.net/dl/' + project + '/' + name + ')'
+                    message += '[Download](https://' + service + '.net/dl/' + project + '/' + file_name + ')'
 
                 if utils.push_notification(message, dry_run):
                     utils.write_to_file(cache_file, list.entries[j].title)
