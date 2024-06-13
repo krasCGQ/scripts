220fd769c0b1ef1043dc619ffbc3153c6bed4c8c
Albert I <kras@raphielgang.org>
2024-06-13 10:18:36 +0800

    setup/termux: Install or update `nano-syntax-highlighting`
    This now brings locally modified nanorc in parity with the original.

--- setup/termux.bash
+++ setup/termux.bash
@@ -33,6 +33,13 @@ pkg update -o Dpkg::Options::="--force-confnew" -y
 prInfo "Installing basic packages..."
 pkg install --no-install-recommends -y "${_basic_pkglist[@]}"
 
+prInfo "Updating nano-syntax-highlighting..."
+wget --https-only -O nano-syntax-highlighting-master.zip -nc \
+    https://github.com/galenguyer/nano-syntax-highlighting/archive/refs/heads/master.zip
+test -d .config/nano/syntax-highlighting || mkdir -p .config/nano/syntax-highlighting
+rm -f .config/nano/syntax-highlighting/*.nanorc  # always clean prior installs
+unzip -j nano-syntax-highlighting-master.zip '*/*.nanorc' -d .config/nano/syntax-highlighting
+
 prInfo "Cloning dotfiles repo..."
 if [[ ! -d .files/.git ]]; then
     rm -rf .files  # ensure nothing gets in our way
@@ -40,9 +47,8 @@ if [[ ! -d .files/.git ]]; then
 fi
 
 prInfo "Creating and fixing nanorc for Termux..."
-mkdir -p .config/nano
-sed "s|include \"/usr|include \"$PREFIX|g;/nano-syntax-highlighting/d;s|\~|$HOME/.files|" \
-    "$HOME/.files/.config/nano/nanorc" >"$HOME/.config/nano/nanorc"
+sed -e "s|/usr/share/nano-s|$HOME/.config/nano/s|;s|^include \"/usr|include \"$PREFIX|g" \
+    -e "s|\~|$HOME/.files|" .files/.config/nano/nanorc >.config/nano/nanorc
 
 prInfo "Moving to Zsh..."
 chsh -s zsh
