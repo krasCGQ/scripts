d6f180d0ca63c7786f898c5cf67ebb6b2e4b1a05
Albert I <kras@raphielgang.org>
2024-06-14 21:05:36 +0800

    setup/termux: Don't uppercase functions and move them after variables
    This can look a bit ugly, and I plan to make it in order I actually want.

--- setup/termux.bash
+++ setup/termux.bash
@@ -8,9 +8,6 @@
 
 set -e
 
-# Modified styled message print from my scripts
-prInfo() { echo "[-] $*"; }
-
 # List of basic packages to be installed
 readonly _basic_pkglist=(
     # Both of these packages have to be installed to have functioning pip.
@@ -24,33 +21,37 @@ readonly _basic_pkglist=(
     'zsh'     # Unix shell
 )
 
+# Mimic Termux's custom message output
+pr_info() { echo "[-] $*"; }
+
+
 # This script is expected to be run on home directory
 cd "$HOME" || exit 1
 
-prInfo "Executing system update..."
+pr_info "Executing system update..."
 pkg update -o Dpkg::Options::="--force-confnew" -y
 
-prInfo "Installing basic packages..."
+pr_info "Installing basic packages..."
 pkg install --no-install-recommends -y "${_basic_pkglist[@]}"
 
-prInfo "Updating nano-syntax-highlighting..."
+pr_info "Updating nano-syntax-highlighting..."
 wget --https-only -O nano-syntax-highlighting-master.zip -nc \
     https://github.com/galenguyer/nano-syntax-highlighting/archive/refs/heads/master.zip
 test -d .config/nano/syntax-highlighting || mkdir -p .config/nano/syntax-highlighting
 rm -f .config/nano/syntax-highlighting/*.nanorc  # always clean prior installs
 unzip -j nano-syntax-highlighting-master.zip '*/*.nanorc' -d .config/nano/syntax-highlighting
 
-prInfo "Cloning dotfiles repo..."
+pr_info "Cloning dotfiles repo..."
 if [[ ! -d .files/.git ]]; then
     rm -rf .files  # ensure nothing gets in our way
     git clone --recurse-submodules https://github.com/krasCGQ/dotfiles.git .files
 fi
 
-prInfo "Creating and fixing nanorc for Termux..."
+pr_info "Creating and fixing nanorc for Termux..."
 sed -e "s|/usr/share/nano-s|$HOME/.config/nano/s|;s|^include \"/usr|include \"$PREFIX|g" \
     -e "s|\~|$HOME/.files|" .files/.config/nano/nanorc >.config/nano/nanorc
 
-prInfo "Moving to Zsh..."
+pr_info "Moving to Zsh..."
 chsh -s zsh
 
-prInfo "Done. Please restart Termux manually."
+pr_info "Done. Please restart Termux manually."
